29efbe57825fb1102d4357f7a61b91b9b31b59a5
aleksey <aalekseyf2001@mail.ru>
2022-10-25 22:39:20 +0300

    lab1 by Fisyuk Aleksey

---
+++ Fisyuk.Aleksey/lab1/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <regex>
+#include "daemon/Daemon.hpp"
+#include <filesystem>
+using namespace std;
+
+int main(int argc, char **argv){
+    try {
+        if(argc!=2){
+            std::cout<<"Not enough arguments"<<std::endl;
+            return -1;
+        }
+        Daemon::getInstance().init(argv[1]);
+        Daemon::getInstance().run();
+    } catch (std::exception const& e){
+        syslog(LOG_ERR, "ERROR:: %s", e.what());
+        Daemon::getInstance().terminate();
+    }
+    return 0;
+}
