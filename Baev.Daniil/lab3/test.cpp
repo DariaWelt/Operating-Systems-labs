08bcd60ea76f12960c3ae93d1d5a8c28d3f35238
Baev Daniil <baev.daniil.2002@gmail.com>
2022-12-22 03:06:45 +0300

    lock free queue test

--- Baev.Daniil/lab3/test.cpp
+++ Baev.Daniil/lab3/test.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
 
 #include "MSMP_queueTest.h"
+#include "lockFreeQueueTest.h"
 
 int main(void){
     bool testRes;
 
-    std::cout << "MSMP Queue:" << std::endl;
+    /*std::cout << "Queue with lock:" << std::endl;
 
     std::cout << "Easy push test" << std::endl;
     testRes = MSMPQueuePushTest(std::size_t(10), 10000, 1);
@@ -39,6 +40,46 @@ int main(void){
 
     std::cout << "Push pop time test" << std::endl;
     testRes = MSMPQueuePushPopTest(10000, 100);
+    if (testRes == true)
+        std::cout << "Sucsess test" << std::endl;
+    else
+        std::cout << "Failed test" << std::endl;*/
+
+    
+
+    std::cout << "Queue lock-free:" << std::endl;
+
+    std::cout << "Easy pop test" << std::endl;
+    testRes = lockFreeQueuePopTest(std::size_t(5), 1000, 1);
+    if (testRes == true)
+        std::cout << "Sucsess test" << std::endl;
+    else
+        std::cout << "Failed test" << std::endl;
+
+    std::cout << "Easy push test" << std::endl;
+    testRes = lockFreeQueuePushTest(std::size_t(5), 1000, 1);
+    if (testRes == true)
+        std::cout << "Sucsess test" << std::endl;
+    else
+        std::cout << "Failed test" << std::endl;
+
+
+    std::cout << "Push time test" << std::endl;
+    testRes = lockFreeQueuePushTest(std::size_t(10), 10000, 100);
+    if (testRes == true)
+        std::cout << "Sucsess test" << std::endl;
+    else
+        std::cout << "Failed test" << std::endl;
+
+    std::cout << "Pop time test" << std::endl;
+    testRes = lockFreeQueuePopTest(std::size_t(10), 10000, 100);
+    if (testRes == true)
+        std::cout << "Sucsess test" << std::endl;
+    else
+        std::cout << "Failed test" << std::endl;
+
+    std::cout << "Push pop time test" << std::endl;
+    testRes = lockFreeQueuePushPopTest(10000, 100);
     if (testRes == true)
         std::cout << "Sucsess test" << std::endl;
     else
