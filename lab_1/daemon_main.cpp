88f11aed2e481b7a9e73573e1e72ef206f66a9d9
yupichkin <darizhapoff2016@gmail.com>
2022-12-27 06:24:33 +0300

    lab 2 commit
    lab 1 commit
    
    lab 1 changes
    
    remove cmake cache
    
    small mistakes fix
    
    small mistakes fix

---
+++ lab_1/daemon_main.cpp
@@ -0,0 +1,18 @@
+
+#include <iostream>
+#include <cstdlib>
+
+#include "src/Daemon.h"
+int main(int argc, char ** argv)
+{
+  if(argc != 2)
+  {
+    std::cout << "Wrong num of arguments" << std::endl;
+    return EXIT_FAILURE;
+  }
+  auto daemon = Daemon::getInstance();
+  daemon->init(argv[1]);
+  auto rc = daemon->execute();
+  delete daemon;
+  return rc;
+}
